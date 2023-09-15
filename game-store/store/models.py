24a59108359f85e1fd986f134369cc1742782ee7
Кирилл <myakiishevkirill@gmail.com>
2023-09-15 17:34:27 +1000

    created models and applied migrations

--- game-store/store/models.py
+++ game-store/store/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Platform(models.Model):
+    name = models.CharField(max_length=255)
+
+    # slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, verbose_name='Url', unique=True)
+    description = models.TextField()
+    release_date = models.DateField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    genres = models.ManyToManyField(Genre)
+    platforms = models.ManyToManyField(Platform)
+    cover_image = models.ImageField(upload_to='game_covers/')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
