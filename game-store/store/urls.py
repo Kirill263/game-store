54986a9c51f721aba249b4dc7b93fc2004ab5c64
Кирилл <myakiishevkirill@gmail.com>
2023-09-29 17:13:37 +1000

    added a template for games by genre

--- game-store/store/urls.py
+++ game-store/store/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', index, name='home'),
     path('genres/', views.all_genres, name='all_genres'),
+    path('genre/<slug:genre_slug>/', views.games_by_genre, name='games_by_genre'),
 ]
--- game-store/store/views.py
+++ game-store/store/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
-from .models import Genre
+from .models import Genre, Game
 
 
 def index(request):
@@ -10,3 +10,9 @@ def index(request):
 def all_genres(request):
     genres = Genre.objects.all()
     return render(request, 'store/genres.html', {'genres': genres})
+
+
+def games_by_genre(request, genre_slug):
+    genre = get_object_or_404(Genre, slug=genre_slug)
+    games = Game.objects.filter(genres__slug=genre_slug)
+    return render(request, 'store/games_by_genre.html', {'genre': genre, 'games': games})
