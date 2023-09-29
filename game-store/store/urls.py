91f558723feefe7527d6bf98a3d5f527ce6e49fd
Кирилл <myakiishevkirill@gmail.com>
2023-09-29 14:53:10 +1000

    added a template for the list of genres

--- game-store/store/urls.py
+++ game-store/store/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import *
+from . import views
 
 urlpatterns = [
     path('', index, name='home'),
+    path('genres/', views.all_genres, name='all_genres'),
 ]
--- game-store/store/views.py
+++ game-store/store/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from .models import Genre
 
 
 def index(request):
     return render(request, 'store/index.html')
+
+
+def all_genres(request):
+    genres = Genre.objects.all()
+    return render(request, 'store/genres.html', {'genres': genres})
